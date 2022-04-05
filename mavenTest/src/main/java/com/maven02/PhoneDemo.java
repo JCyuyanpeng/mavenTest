a14e0bd59acf818acd447e672279ca2415b2abbf
BBC\CB <3288874950@qq.com>
2022-04-05 15:58:41 +0800

    405

---
+++ mavenTest/src/main/java/com/maven02/PhoneDemo.java
@@ -0,0 +1,10 @@
+package com.maven02;
+
+public class PhoneDemo {
+    public static void main(String[] args) {
+//        Phone p = new Phone();
+//        p.call("jack");
+        NewPhone np = new NewPhone();
+        np.call("tom");
+    }
+}

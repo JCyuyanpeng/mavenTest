e11951cd8038df897b2e31ae1b1d9605570f1ad1
BBC\CB <3288874950@qq.com>
2022-04-05 10:22:58 +0800

    new testng

--- mavenTest/src/main/java/com/maven01/HelloWorld.java
+++ mavenTest/src/main/java/com/maven01/HelloWorld.java
@@ -1,8 +1,16 @@
 package com.maven01;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    TestNG02 tg = new TestNG02("tom", 29);
+
+    @Test
+    public void tng(){
+        tg.show();
+        Assert.assertEquals("tom", tg.name, "fail");
+        Assert.assertEquals(29, tg.age, "fail");
     }
 }
